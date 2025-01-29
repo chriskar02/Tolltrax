1e6ec1925f81491158f027b9757eb38ee5d848ee
ntua-el21039 <el21039@mail.ntua.gr>
2025-01-29 16:42:21 +0200

    FRONTEND (HOME PAGE + ADMIN PAGE - 4 BUTTONS)

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
