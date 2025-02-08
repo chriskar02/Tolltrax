7bfa9722376b15673fba16059796ab92a1442120
eleninspl <eleni.nspl@gmail.com>
2025-02-08 15:05:28 +0200

    fix error

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { getPool } = require("../config/db");
+const { getPool } = require("./db");
 
 const router = express.Router();
 const SECRET_KEY = process.env.JWT_SECRET || "your_secret_key";
