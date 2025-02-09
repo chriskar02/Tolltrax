7b41d1ae4ce65b1ceae924d76298d6ed3d6c4e16
ntua-el20130 <el20130@mail.ntua.gr>
2025-02-09 21:32:17 +0200

    diff frontend is ok except for operator, working on it

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,18 +5,18 @@ import OperatorDashboard from './OperatorDashboard';
 import UserDashboard from './UserDashboard';
 
 function Dashboard({ user, setIsAuthenticated }) {
-  // You can customize the following switch however you wish
   switch (user.type) {
-    case 3:
+    case "admin":
       return <AdminDashboard user={user} />;
-    case 2:
+    case "analyst":
       return <AnalystDashboard user={user} setIsAuthenticated={setIsAuthenticated} />;
-    case 1:
+    case "operator":
       return <OperatorDashboard user={user} setIsAuthenticated={setIsAuthenticated} />;
-    case 0:
+    case "normal":
     default:
       return <UserDashboard user={user} setIsAuthenticated={setIsAuthenticated} />;
   }
 }
+
 
 export default Dashboard;
